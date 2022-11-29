b1b781e3b6bed1efea7dbdbc48531d2f499a018e
Chuan <hong.chuan.hannah@gmail.com>
2022-11-29 14:39:47 -0500

    update

--- R/pertubation.R
+++ R/pertubation.R
@@ -5,12 +5,12 @@ ptb_wgt <- 4*rbeta(length(S_labeled), 1/2, 3/2)
 ptb_wgt_unlabeled <- 4*rbeta(sum(is.na(Y)),
                              1/2, 3/2)
 
-# Perturbed estimates.
-if(method=="supervised"){res.bt[[ibt]] <- supervised(S_labeled,Y_labeled, W_labeled = ptb_wgt)
+if(method=="supervised"){res.bt[[ibt]] <- tryCatch(supervised(S_labeled,Y_labeled, W_labeled = ptb_wgt), error=function(e) NA)
 }
-if(method=="ssROC"){res.bt[[ibt]] <- ssROC(S, Y, W_labeled = ptb_wgt,
-                                W_unlabeled = ptb_wgt_unlabeled)
-res.bt[[ibt]]=res.bt[[ibt]][setdiff(ls(res.bt[[ibt]]), "mhat")]
+if(method=="ssROC"){res.bt[[ibt]] <- tryCatch(ssROC(S, Y, W_labeled = ptb_wgt,
+                                W_unlabeled = ptb_wgt_unlabeled),error=function(e) NA)
+
+res.bt[[ibt]]=tryCatch(res.bt[[ibt]][setdiff(ls(res.bt[[ibt]]), "mhat")],error=function(e) NA)
 }
 }
 res.bt
